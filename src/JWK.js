30fbd419d6235899386bcffc11cc1e680e1f263f
Greg Linklater <greglink49@gmail.com>
2017-08-10 17:43:32 +0200

    feat(JWK): skeleton JWK class

---
+++ src/JWK.js
@@ -0,0 +1,24 @@
+'use strict'
+
+/**
+ * Dependencies
+ * @ignore
+ */
+
+/**
+ * Module Dependencies
+ * @ignore
+ */
+
+/**
+ * JWK
+ * @ignore
+ */
+class JWK {
+}
+
+/**
+ * Exports
+ * @ignore
+ */
+module.exports = JWK
