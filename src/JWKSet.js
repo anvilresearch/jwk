0a55a68bf4177b3ca40ba1454de84f0c23aaf9a6
Greg Linklater <greglink49@gmail.com>
2017-08-10 17:43:50 +0200

    feat(JWKSet): skeleton JWKSet class

---
+++ src/JWKSet.js
@@ -0,0 +1,24 @@
+'use strict'
+
+/**
+ * Dependencies
+ * @ignore
+ */
+
+/**
+ * Module Dependencies
+ * @ignore
+ */
+
+/**
+ * JWKSet
+ * @ignore
+ */
+class JWKSet {
+}
+
+/**
+ * Exports
+ * @ignore
+ */
+module.exports = JWKSet
