c5abdc9775770ebd1ff7865a9bb2d5ac9d3ddb3a
Greg Linklater <greglink49@gmail.com>
2017-08-10 17:38:53 +0200

    test(JWK): skeleton JWK test file

---
+++ test/JWKSpec.js
@@ -0,0 +1,34 @@
+'use strict'
+
+/**
+ * Test dependencies
+ * @ignore
+ */
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+
+/**
+ * Assertions
+ * @ignore
+ */
+chai.use(chaiAsPromised)
+chai.should()
+const expect = chai.expect
+
+/**
+ * Code Under Test
+ * @ignore
+ */
+const { JWK } = require('../src')
+
+/**
+ * Test Data
+ * @ignore
+ */
+
+/**
+ * Tests
+ * @ignore
+ */
+describe('JWK', () => {
+})
