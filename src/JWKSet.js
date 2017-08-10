31bd5d845afec8de9a321332d24a84935940d73a
Greg Linklater <greglink49@gmail.com>
2017-08-10 19:37:00 +0200

    feat(JWKSet): JWKSet sketch

--- src/JWKSet.js
+++ src/JWKSet.js
@@ -15,6 +15,79 @@
  * @ignore
  */
 class JWKSet {
+
+  /**
+   * constructor
+   *
+   * @class
+   * JWKSet
+   */
+  constructor (data) {
+    // TODO
+    Object.assign(this, data)
+  }
+
+  /**
+   * generateKeys
+   *
+   * @param  {(String|Object|Array)} data
+   * @return {Promise}
+   */
+  static generateKeys (data) {
+    return Promise.resolve(new JWKSet())
+      .then(jwks => jwks.generateKeys(data))
+  }
+
+  /**
+   * importKeys
+   *
+   * @param  {(String|Object|Array)} data
+   * @return {Promise}
+   */
+  static importKeys (data) {
+    return Promise.resolve(new JWKSet())
+      .then(jwks => jwks.importKeys(data))
+  }
+
+  /**
+   * generateKeys
+   *
+   * @param  {(String|Object|Array)} data
+   * @return {Promise}
+   */
+  generateKeys (data) {
+    return Promise.resolve(this)
+  }
+
+  /**
+   * importKeys
+   *
+   * @param  {(String|Object|Array)} data
+   * @return {Promise}
+   */
+  importKeys (data) {
+    return Promise.resolve(this)
+  }
+
+  /**
+   * filter
+   *
+   * @param  {Function} fn - Filter function
+   * @return {Promise}
+   */
+  filter (fn) {
+    return Promise.resolve(this)
+  }
+
+  /**
+   * find
+   *
+   * @param  {Function} fn - Find function
+   * @return {Promise}
+   */
+  find (fn) {
+    return Promise.resolve(this)
+  }
 }
 
 /**
