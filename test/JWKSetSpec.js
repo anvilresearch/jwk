e450d8abf21f5dabab7105ea24375aa59c065f6e
Greg Linklater <greglink49@gmail.com>
2017-08-11 19:08:34 +0200

    test(JWKSet): JWKSet temporary tests

--- test/JWKSetSpec.js
+++ test/JWKSetSpec.js
@@ -31,4 +31,29 @@ const { JWKSet, JWK } = require('../src')
  * @ignore
  */
 describe('JWKSet', () => {
+
+  it('should generate and import', () => {
+    return JWKSet.generateKeys('RS256').then(jwks => {
+      jwks.keys.length.should.equal(2)
+      jwks.keys[0].should.be.instanceOf(JWK)
+    })
+  })
+
+  it('should generate multiple keys', () => {
+    return JWKSet.generateKeys(['RS256', 'RS256']).then(jwks => {
+      jwks.keys.length.should.equal(4)
+      jwks.keys[3].should.be.instanceOf(JWK)
+    })
+  })
+
+  it('should allow mixed input', () => {
+    return JWKSet.generateKeys(['RS256', { alg: 'RS256', modulusLength: 4096, kid: 'abc', key_ops: ['sign', 'verify'] }]).then(jwks => {
+      jwks.keys.length.should.equal(4)
+      jwks.keys[3].should.be.instanceOf(JWK)
+    })
+  })
+
+  it('should require key_ops in object descriptors', () => {
+    return JWKSet.generateKeys({ alg: 'RS256' }).should.be.rejected
+  })
 })
