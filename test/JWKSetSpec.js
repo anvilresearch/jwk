1b1d100ccf6b90552ed59bb96ddffa90fa717c35
Greg Linklater <greglink49@gmail.com>
2017-08-11 21:05:53 +0200

    test(JWKSet): additional tests

--- test/JWKSetSpec.js
+++ test/JWKSetSpec.js
@@ -32,28 +32,61 @@ const { JWKSet, JWK } = require('../src')
  */
 describe('JWKSet', () => {
 
-  it('should generate and import', () => {
-    return JWKSet.generateKeys('RS256').then(jwks => {
-      jwks.keys.length.should.equal(2)
-      jwks.keys[0].should.be.instanceOf(JWK)
-    })
-  })
+  describe('generateKeys', () => {
 
-  it('should generate multiple keys', () => {
-    return JWKSet.generateKeys(['RS256', 'RS256']).then(jwks => {
-      jwks.keys.length.should.equal(4)
-      jwks.keys[3].should.be.instanceOf(JWK)
-    })
-  })
+    describe('static', () => {
+      it('should return a promise', () => {
+        return JWKSet.generateKeys('RS256').should.be.fulfilled
+      })
 
-  it('should allow mixed input', () => {
-    return JWKSet.generateKeys(['RS256', { alg: 'RS256', modulusLength: 4096, kid: 'abc', key_ops: ['sign', 'verify'] }]).then(jwks => {
-      jwks.keys.length.should.equal(4)
-      jwks.keys[3].should.be.instanceOf(JWK)
-    })
-  })
+      it('should resolve a JWKSet', () => {
+        return JWKSet.generateKeys('RS256').should.eventually.be.a.instanceOf(JWKSet)
+      })
 
-  it('should require key_ops in object descriptors', () => {
-    return JWKSet.generateKeys({ alg: 'RS256' }).should.be.rejected
+      it('should generate and import', () => {
+        return JWKSet.generateKeys('RS256').then(jwks => {
+          jwks.keys.length.should.equal(2)
+          jwks.keys[0].should.be.instanceOf(JWK)
+        })
+      })
+
+      it('should generate multiple keys', () => {
+        return JWKSet.generateKeys(['RS256', 'RS256']).then(jwks => {
+          jwks.keys.length.should.equal(4)
+          jwks.keys[3].should.be.instanceOf(JWK)
+        })
+      })
+
+      it('should allow mixed input', () => {
+        return JWKSet.generateKeys(['RS256', { alg: 'RS256', modulusLength: 4096, kid: 'abc', key_ops: ['sign', 'verify'] }]).then(jwks => {
+          jwks.keys.length.should.equal(4)
+          jwks.keys[3].should.be.instanceOf(JWK)
+        })
+      })
+
+      it('should require key_ops in object descriptors', () => {
+        return JWKSet.generateKeys({ alg: 'RS256' }).should.be.rejected
+      })
+    })
+
+    describe('member', () => {
+      let jwks
+
+      beforeEach(() => {
+        jwks = new JWKSet()
+      })
+
+      it('should return a promise', () => {
+        return jwks.generateKeys('RS256').should.be.fulfilled
+      })
+
+      it('should return newly generatedKeys', () => {
+        return jwks.generateKeys(['RS256', 'RS256'])
+          .then(generated => {
+            generated.length.should.equal(2)
+            jwks.keys.length.should.equal(4)
+          })
+      })
+    })
   })
 })
