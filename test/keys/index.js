57fa75a8e163642aec43854882f84a047f22c373
Greg Linklater <greglink49@gmail.com>
2017-08-11 22:15:07 +0200

    test(keys): RSA keys for testing

---
+++ test/keys/index.js
@@ -0,0 +1,16 @@
+'use strict'
+
+/**
+ * Keys
+ * @ignore
+ */
+const { RSAPrivateJWK, RSAPublicJWK } = require('./RSA')
+
+/**
+ * Exports
+ * @ignore
+ */
+module.exports = {
+  RSAPrivateJWK,
+  RSAPublicJWK
+}
